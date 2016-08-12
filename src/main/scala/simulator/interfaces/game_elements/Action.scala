6db708dbac98a40ef063564e6956eb066ebfa0c1
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-08-12 09:12:36 +0200

    Replaced Move Action by Slide with appropriate stones list.

--- src/main/scala/simulator/interfaces/game_elements/Action.scala
+++ src/main/scala/simulator/interfaces/game_elements/Action.scala
@@ -2,9 +2,11 @@ package simulator.interfaces.game_elements
 
 import simulator.interfaces.game_elements.Direction.Direction
 
+/**
+  * TODO outdated
+  */
 object ActionKind {
   sealed trait ActionKind
-  case object Move extends ActionKind
   case object Slide extends ActionKind
   case object Place extends ActionKind
 }
@@ -12,7 +14,6 @@ object ActionKind {
 sealed trait Action
 
 case class Slide(origin: Position, stones: List[Int], dir: Direction) extends Action
-case class Move(origin: Position, dir: Direction) extends Action
 case class PlaceMinion(origin: Position, pseudo: Boolean = false) extends Action
 case class PlaceWall(origin: Position) extends Action
 case class PlaceCapstone(origin: Position) extends Action
