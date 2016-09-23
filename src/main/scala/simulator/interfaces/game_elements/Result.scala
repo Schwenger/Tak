49d87b47df56f508ddac91752adb851ff1736f30
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-23 22:55:26 +0200

    Made Result a case class.

--- src/main/scala/simulator/interfaces/game_elements/Result.scala
+++ src/main/scala/simulator/interfaces/game_elements/Result.scala
@@ -3,4 +3,4 @@ package simulator.interfaces.game_elements
 import simulator.interfaces.GameState
 import simulator.interfaces.PlayerColor.PlayerColor
 
-class Result(winner: PlayerColor, state: GameState)
+case class Result(winner: PlayerColor, state: GameState)
