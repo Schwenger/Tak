95b7e96337f9345c0981a65b854f82ed51f90f41
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-23 14:48:57 +0200

    Extended Error trait to capture compile error as well.

--- src/main/scala/parsing/action/ActionInputError.scala
+++ src/main/scala/parsing/action/ActionInputError.scala
@@ -1,5 +1,6 @@
 package parsing.action
 
-trait ActionInputError
+trait ActionInputError extends Throwable
 case class ActionLexerError(msg: String) extends ActionInputError
 case class ActionParserError(msg: String) extends ActionInputError
+case class ActionCompilerError(msg: String) extends ActionInputError
