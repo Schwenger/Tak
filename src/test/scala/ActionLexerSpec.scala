6bfb6bc939e835e9d9793644ecfd3afb09513e6a
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-23 22:57:11 +0200

    Extended ActionLexerSpec to make sure slides can be lexed correctly.

--- src/test/scala/ActionLexerSpec.scala
+++ src/test/scala/ActionLexerSpec.scala
@@ -6,8 +6,8 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   "ActionLexer" should "lex correctly" in {
     val input = "move place at from 3 position )"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (7)
     tokens(0) shouldBe MOVECMD
     tokens(1) shouldBe PLACECMD
@@ -21,17 +21,47 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   it should "lex numbers" in {
     val input = "3"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (1)
     tokens.head shouldBe NUMBER("3")
+  }
+
+  it should "lex number sequences" in {
+    val input = "3, 3"
+    val lexRes = Lexer(input)
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
+    tokens.length should be (3)
+    tokens.head shouldBe NUMBER("3")
+    tokens(1) shouldBe COMMA
+    tokens(2) shouldBe NUMBER("3")
+  }
+
+  it should "lex slides" in {
+    val input = "slide (3,1) north drop 7,5"
+    val lexRes = Lexer(input)
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
+    tokens.length should be (11)
+    tokens(0) shouldBe SLIDECMD
+    tokens(1) shouldBe LPAR
+    tokens(2) shouldBe NUMBER("3")
+    tokens(3) shouldBe COMMA
+    tokens(4) shouldBe NUMBER("1")
+    tokens(5) shouldBe RPAR
+    tokens(6) shouldBe NORTH
+    tokens(7) shouldBe DROP
+    tokens(8) shouldBe NUMBER("7")
+    tokens(9) shouldBe COMMA
+    tokens(10) shouldBe NUMBER("5")
   }
 
   it should "lex surrenders" in {
     val input = "surrender"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (1)
     tokens.head shouldBe SURRENDER
   }
@@ -39,8 +69,8 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   it should "lex parenthesis" in {
     val input = "("
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (1)
     tokens.head shouldBe LPAR
   }
@@ -48,8 +78,8 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   it should "lex at" in {
     val input = "at"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (1)
     tokens.head shouldBe AT
   }
@@ -57,8 +87,8 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   it should "lex more than one item" in {
     val input = "at to"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (2)
     tokens.head shouldBe AT
     tokens(1) shouldBe TO
@@ -67,8 +97,8 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   it should "ignore cases" in {
     val input = "at PoSiTion tO"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (3)
     tokens.head shouldBe AT
     tokens(1) shouldBe POSITION
@@ -78,8 +108,8 @@ class ActionLexerSpec extends FlatSpec with Matchers {
   it should "allow a mix of numbers and keywords" in {
     val input = "at 3 to"
     val lexRes = Lexer(input)
-    lexRes.isRight should be (true)
-    val tokens = lexRes.right.get
+    lexRes shouldBe 'isSuccess
+    val tokens: Seq[ActionParseToken] = lexRes.get
     tokens.length should be (3)
     tokens.head shouldBe AT
     tokens(1) shouldBe NUMBER("3")
