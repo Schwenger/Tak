c11b8ca978af43a7b84b1b27dd4fe89e1a2170c0
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-24 01:54:12 +0200

    First draft of a main class.

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,16 @@
+import interfaces.{CLI, Interface}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val interfaceSpec = args(0)
+    val interface = interfaceSpec match {
+      case "-cli" => new CLI()
+      case "-gui" => ???
+      case "-api" => ???
+    }
+    interface.greet()
+    interface.play()
+  }
+
+}
