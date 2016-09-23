9a5a5d17d82d39c80fad933bd181ed835f470f35
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-23 22:50:22 +0200

    Enriched error by message and tostring method.

--- src/main/scala/parsing/action/ActionInputError.scala
+++ src/main/scala/parsing/action/ActionInputError.scala
@@ -1,6 +1,8 @@
 package parsing.action
 
-trait ActionInputError extends Throwable
-case class ActionLexerError(msg: String) extends ActionInputError
-case class ActionParserError(msg: String) extends ActionInputError
-case class ActionCompilerError(msg: String) extends ActionInputError
+class ActionInputError(msg: String) extends Throwable {
+  override def toString = msg
+}
+case class ActionLexerError(msg: String) extends ActionInputError(msg)
+case class ActionParserError(msg: String) extends ActionInputError(msg)
+case class ActionCompilerError(msg: String) extends ActionInputError(msg)
