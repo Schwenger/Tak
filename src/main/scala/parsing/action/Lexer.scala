41386aba59fa0de44beb81efe3bc5193441cbd6e
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-23 14:50:09 +0200

    Minorly extended tokens, adapted lexer.

--- src/main/scala/parsing/action/Lexer.scala
+++ src/main/scala/parsing/action/Lexer.scala
@@ -13,32 +13,36 @@ object Lexer extends RegexParsers {
     case Success(result, next) => Right(result)
   }
 
-  def number: Parser[NUMBER] = "[0-9]".r ^^ { s => NUMBER(s.toInt) }
+  def number: Parser[NUMBER] = "[0-9]".r ^^ { s => NUMBER(s) }
 
-  def surrender = "surrender|giveup|forfeit".r ^^ (_ => SURRENDER)
-  def placecmd  = "place|set".r ^^ (_ => PLACECMD)
-  def position  = "position" ^^ (_ => POSITION)
-  def capstone  = "capstone" ^^ (_ => CAPSTONE)
-  def minion    = "minion"   ^^ (_ => MINION)
-  def north     = "north"    ^^ (_ => NORTH)
-  def south     = "south"    ^^ (_ => SOUTH)
-  def wall      = "wall"     ^^ (_ => WALL)
-  def movecmd   = "move"     ^^ (_ => MOVECMD)
-  def take      = "take"     ^^ (_ => TAKE)
-  def drop      = "drop"     ^^ (_ => DROP)
-  def from      = "from"     ^^ (_ => FROM)
-  def west      = "west"     ^^ (_ => WEST)
-  def east      = "east"     ^^ (_ => EAST)
-  def at        = "at"       ^^ (_ => AT)
-  def to        = "to"       ^^ (_ => TO)
-  def lpar      = "("        ^^ (_ => LPAR)
-  def rpar      = ")"        ^^ (_ => RPAR)
-  def comma     = ","        ^^ (_ => COMMA)
+  def surrender = "surrender|give up|forfeit".r ^^ (_ => SURRENDER)
+  def east      = "east|right".r ^^ (_ => EAST)
+  def south     = "south|down".r ^^ (_ => SOUTH)
+  def placecmd  = "place|set".r  ^^ (_ => PLACECMD)
+  def west      = "west|left"    ^^ (_ => WEST)
+  def north     = "north|up"     ^^ (_ => NORTH)
+  def position  = "position"     ^^ (_ => POSITION)
+  def capstone  = "capstone"     ^^ (_ => CAPSTONE)
+  def minion    = "minion"       ^^ (_ => MINION)
+  def stack     = "stack"        ^^ (_ => STACK)
+  def slidecmd  = "slide"        ^^ (_ => SLIDECMD)
+  def wall      = "wall"         ^^ (_ => WALL)
+  def movecmd   = "move"         ^^ (_ => MOVECMD)
+  def take      = "take"         ^^ (_ => TAKE)
+  def drop      = "drop"         ^^ (_ => DROP)
+  def from      = "from"         ^^ (_ => FROM)
+  def at        = "at"           ^^ (_ => AT)
+  def to        = "to"           ^^ (_ => TO)
+  def my        = "my"           ^^ (_ => MY)
+  def a         = "a"            ^^ (_ => A)
+  def lpar      = "("            ^^ (_ => LPAR)
+  def rpar      = ")"            ^^ (_ => RPAR)
+  def comma     = ","            ^^ (_ => COMMA)
 
   def tokens: Parser[List[Token]] = {
     phrase(rep1(
       number | surrender | placecmd | movecmd | position | capstone | minion | north | south | wall | take | drop |
-      from | west | east | at | to | lpar | rpar | comma
+      from | west | east | at | to | lpar | rpar | comma | stack | slidecmd | a | my
     ))
   }
 
--- src/main/scala/parsing/action/Token.scala
+++ src/main/scala/parsing/action/Token.scala
@@ -3,23 +3,27 @@ package parsing.action
 sealed trait Token
 
 case object SURRENDER extends Token
-case object MOVECMD extends Token
-case object PLACECMD extends Token
-case object CAPSTONE extends Token
-case object MINION extends Token
-case object WALL extends Token
-case object POSITION extends Token
-case object LPAR extends Token
-case object RPAR extends Token
-case object COMMA extends Token
-case object AT extends Token
-case object TO extends Token
-case object TAKE extends Token
-case object DROP extends Token
-case object FROM extends Token
-case object NORTH extends Token
-case object SOUTH extends Token
-case object WEST extends Token
-case object EAST extends Token
+case object PLACECMD  extends Token
+case object SLIDECMD  extends Token
+case object CAPSTONE  extends Token
+case object POSITION  extends Token
+case object MOVECMD   extends Token
+case object MINION    extends Token
+case object STACK     extends Token
+case object COMMA     extends Token
+case object NORTH     extends Token
+case object SOUTH     extends Token
+case object LPAR      extends Token
+case object RPAR      extends Token
+case object TAKE      extends Token
+case object WEST      extends Token
+case object EAST      extends Token
+case object WALL      extends Token
+case object DROP      extends Token
+case object FROM      extends Token
+case object MY        extends Token
+case object AT        extends Token
+case object TO        extends Token
+case object A         extends Token
 
-case class NUMBER(value: Int) extends Token
+case class NUMBER(value: String) extends Token
