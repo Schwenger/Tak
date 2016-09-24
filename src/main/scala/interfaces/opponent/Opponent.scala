1d36c78d0b786eef4646378a20dd9b36a729701b
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-24 12:04:51 +0200

    Right now, all opponents are random players due to a lack of other implementations.

--- src/main/scala/interfaces/opponent/Opponent.scala
+++ src/main/scala/interfaces/opponent/Opponent.scala
@@ -1,6 +1,8 @@
 package interfaces.opponent
 
+import ai.players.RandomPlayer
 import simulator.interfaces.Player
+import simulator.interfaces.PlayerColor.PlayerColor
 
 abstract class Opponent(val name: String, vit: Int, str: Int, int: Int, dex: Int) {
   val overall: Int = (vit + str + int + dex) / 4
@@ -17,37 +19,37 @@ abstract class Opponent(val name: String, vit: Int, str: Int, int: Int, dex: Int
 
   private def prop(v: Int): String = full * v + empty * (max - v)
 
-  def toPlayer: Player
+  def toPlayer(color: PlayerColor, boardSize: Int): Player
 }
 
 object Opponent {
   val all: List[Opponent] = List(
-    Caterpillar, Aiori, Wisely, LeftShark, Orc, Kvoth, Jesus, Paul
+    Caterpillar, Auri, Wisely, LeftShark, Orc, Kvoth, Jesus, Paul
   ).sortBy(_.overall)
   def maxNameLength: Int = (all map (_.name.length)).max
 }
 
 object Caterpillar extends Opponent("The Very Hungry Caterpiller", 4, 1, 1, 1){
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
-object Aiori extends Opponent("Aiori", 1, 1, 2, 5){
-  override def toPlayer = ???
+object Auri extends Opponent("Auri", 1, 1, 2, 5){
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
 object Wisely extends Opponent("Wisely", 2, 1, 5, 3){
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
 object LeftShark extends Opponent("Left Shark", 4, 3, 1, 2) {
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
 object Orc extends Opponent("Orc", 4, 5, 1, 2){
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
 object Kvoth extends Opponent("Kvoth", 3, 4, 5, 4){
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
 object Jesus extends Opponent("Jesus H. Christ", 5, 5, 4, 5){
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
 object Paul extends Opponent("Wolfgang J. Paul", 5, 5, 5, 5){
-  override def toPlayer = ???
+  override def toPlayer(color: PlayerColor, boardSize: Int) = new RandomPlayer(color, boardSize)
 }
