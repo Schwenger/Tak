dbc2c98d1c8ad5022ca83b953abbc87541be399a
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-24 23:54:38 +0200

    Introduced super class for debug modes.

---
+++ src/main/scala/interfaces/debugmode/DebugMode.scala
@@ -0,0 +1,36 @@
+package interfaces.debugmode
+
+import ai.players.MinMaxPlayer
+import simulator.interfaces.{Player, PlayerColor, Simulator}
+
+abstract class DebugMode {
+
+  protected def apply(size: Int, runs: Int, red: Player, black: Player) = {
+
+    println(s"Config: runs: $runs (dft: 1); size: $size (dft: 4)")
+    println("Instantiating RandomPlayer.")
+
+    val start = System.currentTimeMillis()
+    for(i <- 0 until runs) {
+      println(s"Starting Simulation $i.")
+      Simulator(red, black, size)
+    }
+    val stop = System.currentTimeMillis()
+
+    println(s"Simulation terminated successfully. Time elapsed for $runs simulations: ${stop - start}")
+    println(s"Red won: ${Simulator.won._1}, Black won: ${Simulator.won._2}.")
+  }
+
+  protected def parseCommandLine(args: Seq[String]): (Int, Int) = {
+    var size = 4
+    var runs = 1
+    for(i <- args.indices by 2) {
+      args(i) match {
+        case "-size" | "-s" => size = args(i+1).toInt
+        case "-runs" | "-r" => runs = args(i+1).toInt
+      }
+    }
+    (size, runs)
+  }
+
+}
--- src/main/scala/interfaces/debugmode/RandomPlayerMode.scala
+++ src/main/scala/interfaces/debugmode/RandomPlayerMode.scala
@@ -1,35 +1,17 @@
 package interfaces.debugmode
 
+import ai.evaluation.TokenCount
 import ai.players.RandomPlayer
-import simulator.interfaces.{PlayerColor, Simulator}
+import simulator.interfaces.PlayerColor
 
-object RandomPlayerMode {
+object RandomPlayerMode extends DebugMode {
 
-  def apply(args: Seq[String]): Unit = {
-    var size = 4
-    var runs = 1
-    for(i <- args.indices by 2) {
-      args(i) match {
-        case "-size" | "-s" => size = args(i+1).toInt
-        case "-runs" | "-r" => runs = args(i+1).toInt
-      }
-    }
-
-    println(s"Config: runs: $runs (dft: 1); size: $size (dft: 4)")
-
-    println("Instantiating RandomPlayer.")
+  def apply(args: Seq[String]) = {
+    val (size, runs) = super.parseCommandLine(args)
     val red = new RandomPlayer(PlayerColor.Red, size)
     val black = new RandomPlayer(PlayerColor.Black, size)
 
-    val start = System.currentTimeMillis()
-    for(i <- 0 until runs) {
-      println(s"Starting Simulation $i.")
-      Simulator(red, black, size)
-    }
-    val stop = System.currentTimeMillis()
-
-    println(s"Simulation terminated successfully. Time elapsed for $runs simulations: ${stop - start}")
-    println(s"Red won: ${Simulator.won._1}, Black won: ${Simulator.won._2}.")
+    super.apply(size, runs, red, black)
 
   }
 
