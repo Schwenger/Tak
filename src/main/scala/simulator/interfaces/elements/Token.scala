54c54a58b2cd56b5dd95ad02bf27b04eb54de739
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-24 21:54:41 +0200

    Improved maintainablility in token's stack method and fixed mix-up.

--- src/main/scala/simulator/interfaces/elements/Token.scala
+++ src/main/scala/simulator/interfaces/elements/Token.scala
@@ -13,10 +13,10 @@ sealed abstract class Token(val player: PlayerColor, val blocking: Boolean, val 
     * Stacks the other Token's content on top of this Token's content. Creates a Stack in the process.
     * The own content might be modified in case it is topped by a Wall and crushed by a Capstone.
     * The Capstone can potentially be part of a Stack.
-    * @param other: token to be stacked on top of this Token.
+    * @param top: token to be stacked on top of this Token.
     * @return
     */
-  def ::(other: Token): Token
+  def ::(top: Token): Token
 }
 
 object Tokenizer {
@@ -30,12 +30,12 @@ object Tokenizer {
 case class Wall(p: PlayerColor) extends Token(p, blocking = true, crushing = false, crushable = true,
   streetable = false, worthAPoint = false) {
 
-  override def ::(other: Token): Token = {
-    assert(other.crushing)
+  override def ::(top: Token): Token = {
+    assert(top.crushing)
     val crushed = Minion(player)
-    other match {
+    top match {
       case Stack(content) => Stack(content :+ crushed)
-      case Capstone(_) => Stack(List(other, crushed))
+      case Capstone(_) => Stack(List(top, crushed))
       case _ => throw new UnsupportedOperationException("This is not allowed at all.")
     }
   }
@@ -44,11 +44,10 @@ case class Wall(p: PlayerColor) extends Token(p, blocking = true, crushing = fal
 case class Minion(p: PlayerColor) extends Token(player = p, blocking = false, crushing = false, crushable = true,
   streetable = true, worthAPoint = true) {
 
-  override def ::(other: Token): Token = {
-    assert(!other.blocking)
-    other match {
-      case Stack(content) => Stack(content :+ this)
-      case _ => Stack(List(other, this))
+  override def ::(top: Token): Token = {
+    top match {
+      case Stack(content) => Stack(content ++ List(this))
+      case _ => Stack(List(top, this))
     }
   }
 }
@@ -59,15 +58,15 @@ case class Stack(content: List[Token]) extends Token(player = content.head.playe
 
   assert(!content.tail.exists(t => t.isInstanceOf[Capstone] || t.isInstanceOf[Wall]))
 
-  override def ::(other: Token): Token = {
-    assert(!this.blocking || this.crushable && other.crushing)
-    var top = this.content
+  override def ::(top: Token): Token = {
+    assert(!this.blocking || this.crushable && top.crushing)
+    var botList = this.content
     if(this.blocking) {
-      top = Minion(this.content.head.player) :: this.content.tail
+      botList = Minion(this.content.head.player) :: this.content.tail
     }
-    other match {
-      case Stack(otherContent) => Stack(otherContent ::: top)
-      case _ => Stack(other :: top)
+    top match {
+      case Stack(otherContent) => Stack(otherContent ::: botList)
+      case _ => Stack(top :: botList)
     }
   }
 }
@@ -75,7 +74,7 @@ case class Stack(content: List[Token]) extends Token(player = content.head.playe
 case class Capstone(p: PlayerColor) extends Token(player = p, blocking = true, crushing = true, crushable = false,
   streetable = true, worthAPoint = false) {
 
-  override def ::(other: Token): Token = {
+  override def ::(top: Token): Token = {
     throw new IllegalArgumentException // nothing can go on top of a Capstone
   }
 }
