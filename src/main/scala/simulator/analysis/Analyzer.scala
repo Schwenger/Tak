6aeff4e84d72e5eb27a2dd7002ea23ca12139e14
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-25 18:48:57 +0200

    Moved and polished game over mechanic.

--- src/main/scala/simulator/analysis/Analyzer.scala
+++ src/main/scala/simulator/analysis/Analyzer.scala
@@ -1,7 +1,9 @@
 package simulator.analysis
 
 import simulator.{GameState, PlayerMapping}
-import simulator.PlayerColor.{Black, Red}
+import simulator.PlayerColor.{Black, PlayerColor, Red}
+import simulator.elements.Direction.{Direction, Down, Up}
+import simulator.elements.{Direction, Position}
 
 object Analyzer {
 
@@ -15,5 +17,48 @@ object Analyzer {
         case None => accu
       }
     )
+
+  def minStreetDist(state: GameState, hor: Boolean): PlayerMapping[Int] = {
+    type OpenList = List[Position]
+    def run(state: GameState, color: PlayerColor, dir: Direction, init: Int => Position): Int = {
+      val (o1, o2) = dir.orth
+
+      def qualifyingPos(pos: Position): Boolean =
+        pos.valid(state.size) && state(pos).exists(_.streetable) && state.dominatedBy(pos, color)
+
+      def getInitialOpenList(color: PlayerColor, init: Int => Position): OpenList = {
+        (Range(0, state.size) collect { case i if qualifyingPos(init(i)) => init(i) }).toList
+      }
+
+      def step(open: OpenList, dir: Direction): OpenList = {
+
+        def expandForward(pos: Position): Option[Position] = dir(pos) match {
+          case next if qualifyingPos(next) => Some(next)
+          case _ => None
+        }
+        def expandOrth(pos: Position): OpenList = {
+          def expandInDir(pos: Position, dir: Direction): OpenList = dir(pos) match {
+            case next if qualifyingPos(next) => next :: expandInDir(next, dir)
+            case _ => Nil
+          }
+          expandInDir(pos, o1) ::: expandInDir(pos, o2)
+        }
+
+        open flatMap expandForward flatMap (next => next :: expandOrth(next))
+      }
+      val open = getInitialOpenList(color, init)
+      val openLists = Range(0, state.size - 1).foldLeft(List(open))((accu, i) => step(accu.head, dir) :: accu)
+      openLists.indexWhere(_.nonEmpty)
+    }
+
+    val (dir, init): (Direction, Int => Position) = hor match {
+      case true => (Direction.Right, i => Position(0, i))
+      case false => (Direction.Up, i => Position(i, 0))
+    }
+
+    val red = run(state, Red, dir, init)
+    val black = run(state, Black, dir, init)
+    PlayerMapping(red, black)
+  }
 
 }
--- src/main/scala/simulator/logic/GameOver.scala
+++ src/main/scala/simulator/logic/GameOver.scala
@@ -16,12 +16,12 @@ object GameOver {
   def apply(state: GameState) : Option[PlayerColor] = {
     if(state.surrendered.isDefined)
       return state.surrendered
-    val horizontal = hasStreet_impl1(state, Direction.Right, i => Position(0, i))
-    val vertical = hasStreet_impl1(state, Direction.Up, i => Position(i, 0))
-    (horizontal.red || vertical.red, horizontal.black || vertical.black) match {
+    val hor  = Analyzer.minStreetDist(state, hor = true)  map { (r,b) => (r == 0, b == 0) }
+    val vert = Analyzer.minStreetDist(state, hor = false) map { (r,b) => (r == 0, b == 0) }
+    (hor.red || vert.red, hor.black || vert.black) match {
       case (true, false) => Some(Red)
       case (false, true) => Some(Black)
-      case (true, true) => println("lol"); Some(Red) //throw new IllegalStateException("This case must not occur.")
+      case (true, true) => println("lol"); Some(Red)
       case (false, false) => fullBoard(state)
     }
   }
@@ -40,44 +40,6 @@ object GameOver {
       case  1 => Some(Red)
       case -1 => Some(Black)
     }
-  }
-
-  def hasStreet_impl1(state: GameState, dir: Direction, initial: Int => Position): PlayerMapping[Boolean] = {
-    val (orth1, orth2) = dir.orth
-
-    def qualifyingPos(pos: Position)(implicit color: PlayerColor): Boolean =
-      pos.valid(state.size) && state.dominatedBy(pos, color) && state(pos).exists(_.streetable)
-
-    def qualifyingField(field: Option[Token], color: PlayerColor): Boolean =
-      field exists (token => token.player == color && token.streetable)
-
-    def nextStep(open: Seq[Position])(implicit color: PlayerColor): Seq[Position] =
-      open collect { case pos if qualifyingField(state(dir(pos)), color) => dir(pos) }
-
-    def expand(open: Seq[Position])(implicit color: PlayerColor): Seq[Position] = {
-      def move(pos: Position, dir: Direction): Seq[Position] = {
-        if (qualifyingPos(pos))
-          move(dir(pos), dir) :+ pos
-        else
-          Nil
-      }
-      open.foldLeft(Nil: Seq[Position]) { (accu: Seq[Position], pos: Position) =>
-        val move1 = move(pos, orth1)
-        val move2 = move(pos, orth2)
-        move1 ++ move2 ++ accu
-      }.distinct
-    }
-    def checkBoard(implicit color: PlayerColor) = {
-      var open: Seq[Position] = for {i <- 0 until state.size
-                                     if state.dominatedBy(initial(i), color)} yield initial(i)
-      for(i <- 1 until state.size){
-        open = expand(nextStep(open))
-      }
-      open
-    }
-    val red = checkBoard(Red)
-    val black = checkBoard(Black)
-    PlayerMapping(red.nonEmpty, black.nonEmpty)
   }
 
 }
