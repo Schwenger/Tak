993a6d167f95c9420984c26b647c3267776c1c0e
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-25 15:05:34 +0200

    Introduced interpolator allowing to interpolate between several evaluators.

---
+++ src/main/scala/ai/evaluation/Interpolator.scala
@@ -0,0 +1,16 @@
+package ai.evaluation
+
+import simulator.interfaces.PlayerColor.PlayerColor
+
+class Interpolator(weightedEvaluators: Seq[(Evaluator, Double)]) extends Evaluator {
+
+  override def apply(color: PlayerColor): Eval = {
+
+    val eval: Seq[(Eval, Double)] = weightedEvaluators map { tup => (tup._1(color), tup._2) }
+
+    state =>
+      (eval map {tup => tup._1(state) * tup._2 }).sum
+
+  }
+
+}
