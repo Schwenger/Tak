68f35b49bb46b3306f96df342a1390a52cc06cf4
Maximilian Schwenger <schwengermaximilian@gmail.com>
2016-09-26 22:01:55 +0200

    Adapted test to recent changes.

--- src/test/scala/MinMaxSpec.scala
+++ src/test/scala/MinMaxSpec.scala
@@ -1,5 +1,5 @@
 
-import ai.evaluation.{Domination, GoalIndicator}
+import ai.evaluation.{DefaultInterpolator, Domination, GoalIndicator}
 import ai.search.MinMax
 import org.scalatest._
 import parsing.state.StateDeserializer
@@ -25,18 +25,19 @@ class MinMaxSpec extends FlatSpec with Matchers {
 
       val supplier: (GameState, PlayerColor) => Seq[Action] = (state, color) => ActionSupplier(state)(color)
 
+      val eval = DefaultInterpolator.allEqual
+
     }
 
   "MinMax" should "place a minion to increase the dominance" in {
     val f = fixture
     val c = Red
-    val eval = Domination(c)
     val stateStr =
       """
         | RM & RM
         | RM & _
       """.stripMargin
-    val action = MinMax(f.state(stateStr), eval, f.supplier, 1, c)
+    val action = MinMax(f.state(stateStr), f.eval(c), f.supplier, 1, c)
     val resSpace = List(PlaceMinion(Position(1,0)), PlaceWall(Position(1,0)))
     resSpace should contain (action)
   }
@@ -44,14 +45,13 @@ class MinMaxSpec extends FlatSpec with Matchers {
   it should "output the winning move for red with depth 1" in {
     val f = fixture
     val c = Red
-    val eval = GoalIndicator(c)
 
     val stateStr1 =
       """
         | RM & _
         | BM & _
       """.stripMargin
-    val a1 = MinMax(f.state(stateStr1), eval, f.supplier, 1, c)
+    val a1 = MinMax(f.state(stateStr1), f.eval(c), f.supplier, 1, c)
     val resSpace1 = List(PlaceMinion(Position(1,1)), PlaceWall(Position(1,1)))
     resSpace1 should contain (a1)
 
@@ -60,7 +60,7 @@ class MinMaxSpec extends FlatSpec with Matchers {
         | BM & _
         | RM & _
       """.stripMargin
-    val a2 = MinMax(f.state(stateStr2), eval, f.supplier, 1, c)
+    val a2 = MinMax(f.state(stateStr2), f.eval(c), f.supplier, 1, c)
     a2 shouldBe PlaceMinion(Position(1,0))
 
     val stateStr3 =
@@ -68,7 +68,7 @@ class MinMaxSpec extends FlatSpec with Matchers {
         | RM & BM
         | _  & _
       """.stripMargin
-    val a3 = MinMax(f.state(stateStr3), eval, f.supplier, 1, c)
+    val a3 = MinMax(f.state(stateStr3), f.eval(c), f.supplier, 1, c)
     a3 shouldBe PlaceMinion(Position(0,0))
 
     val stateStr4 =
@@ -76,7 +76,7 @@ class MinMaxSpec extends FlatSpec with Matchers {
         | BM & RM
         | _ & _
       """.stripMargin
-    val a4 = MinMax(f.state(stateStr4), eval, f.supplier, 1, c)
+    val a4 = MinMax(f.state(stateStr4), f.eval(c), f.supplier, 1, c)
     val resSpace4 = List(PlaceMinion(Position(1,0)), PlaceWall(Position(1,0)))
     resSpace4 should contain (a4)
   }
@@ -84,7 +84,6 @@ class MinMaxSpec extends FlatSpec with Matchers {
   it should "prevent a loss by blocking the enemy's road" in {
     val f = fixture
     val c = Red
-    val eval = GoalIndicator(c)
 
     val stateStr1 =
       """
@@ -92,7 +91,7 @@ class MinMaxSpec extends FlatSpec with Matchers {
         | _  & _  & _
         | _  & BM & _
       """.stripMargin
-    val a1 = MinMax(f.state(stateStr1), eval, f.supplier, 2, c)
+    val a1 = MinMax(f.state(stateStr1), f.eval(c), f.supplier, 2, c)
     val resSpace = List(PlaceMinion(Position(1,1)), PlaceWall(Position(1,1)))
     resSpace should contain (a1)
   }
@@ -100,7 +99,6 @@ class MinMaxSpec extends FlatSpec with Matchers {
   it should "prevent a loss sliding in the way even though this means losing dominance" in {
     val f = fixture
     val c = Red
-    val eval = GoalIndicator(c)
 
     val stateStr1 =
       """
@@ -111,7 +109,7 @@ class MinMaxSpec extends FlatSpec with Matchers {
     val state = f.state(stateStr1)
     for (i <- 0 until state.minionsLeft(c))
       state.removeToken(c, minion = true)
-    val a1 = MinMax(state, eval, f.supplier, 2, c)
+    val a1 = MinMax(state, f.eval(c), f.supplier, 2, c)
     a1 should be (Slide(Position(2, 0), List(2,1), Direction.Up))
   }
 
